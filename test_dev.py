251e884c788549a3e1c5b228b5f4ed408626c8b9
Farha Parveen <farhaparveen919@gmail.com>
2024-04-30 16:01:20 -0700

    Updated the testing file

--- test_dev.py
+++ test_dev.py
@@ -1,7 +1,12 @@
 print("Hello"+"World")
 
+i = [5,4,3]
+j = [1]
+i.append(j)
+print("The result = ",i)
+
 a=10
 b=50
 
 a,b=b,a
- print("The value of  A = ",a,"\n","The value of B = ",b) 
+print("The value of  A = ",a,"\n","The value of B = ",b) 
---
+++ update_test_dev.py
@@ -0,0 +1,6 @@
+i = [5,6,7]
+j= [1]
+i.append(j)
+i.pop()
+
+print("The result = {}".format(i))
